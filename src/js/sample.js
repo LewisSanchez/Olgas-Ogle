c37881f0409853ad0ccd7a73fabbeeb1a5caa374
StewartTaylor <stewart.taylor@oit.edu>
2015-10-07 15:17:15 -0400

    Added move validation and highlighting of legal chess moves.

--- src/js/sample.js
+++ src/js/sample.js
@@ -1,7 +1,7 @@
 var init = function() {
 
 //--- start example JS ---
-var board1 = new ChessBoard('board1', 'start');
+var board = new ChessBoard('board', 'start');
 //var ruyLopez = 'r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R';
 //var board1 = ChessBoard('board1', ruyLopez);
 //--- end example JS ---
