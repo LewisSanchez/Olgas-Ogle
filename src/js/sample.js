e819c9b9993c11fb6e2711e80373df72ab062deb
StewartTaylor <stewart.taylor@oit.edu>
2015-10-07 02:54:11 -0400

    Fixed pieces not appearing on chessboard.  Path to img was wrong and I needed window.onload to wait for all images to load.

--- src/js/sample.js
+++ src/js/sample.js
@@ -1,1 +1,13 @@
-var board1 = ChessBoard('board1', 'start');
+var init = function() {
+
+//--- start example JS ---
+var board1 = new ChessBoard('board1', 'start');
+//var ruyLopez = 'r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R';
+//var board1 = ChessBoard('board1', ruyLopez);
+//--- end example JS ---
+
+}; // end init()
+
+//$(document).ready(init);
+
+window.onload = init
